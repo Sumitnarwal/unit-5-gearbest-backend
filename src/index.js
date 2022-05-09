4e78edc7294d9644745dea3e261beabceced302e
Sumitnarwal <sumitnarwal39@gmail.com>
2022-05-09 11:53:47 +0530

    sdf

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
-const express=require("express");
-const cors=require("cors");
-const  connect  = require("./configs/db");
-const app=express();
-const port =process.env.Port || 7005
+const express = require("express");
+const cors = require("cors");
+const connect = require("./configs/db");
+const app = express();
+const port = process.env.Port
 app.use(express.json())
 app.use(cors());
-const allController=require("./controller/allpro.controllers")
-const kitchenController=require("./controller/kitchen.contro")
-const laptopController=require("./controller/laptop.contro")
-const mobileController=require("./controller/mobile.con")
-const outdoorController=require("./controller/outdoor.contr")
-const scienceController=require("./controller/science.controll")
-const userController=require("./controller/collectionController")
-const addtoCartController=require("./controller/adToCartController")
-const superDealController=require("./controller/superDealContro")
-const placeorderController=require("./controller/placeorder.controller")
-const {login,register}=require("./controller/auth.controller")
+const allController = require("./controller/allpro.controllers")
+const kitchenController = require("./controller/kitchen.contro")
+const laptopController = require("./controller/laptop.contro")
+const mobileController = require("./controller/mobile.con")
+const outdoorController = require("./controller/outdoor.contr")
+const scienceController = require("./controller/science.controll")
+const userController = require("./controller/collectionController")
+const addtoCartController = require("./controller/adToCartController")
+const superDealController = require("./controller/superDealContro")
+const placeorderController = require("./controller/placeorder.controller")
+const { login, register } = require("./controller/auth.controller")
 
 
 
 
-app.use("/superDeal",superDealController)
-app.use("/addtocart",addtoCartController)
-app.use("/collection",userController)
-app.use("/all",allController)
-app.use("/kitchen",kitchenController)
-app.use("/laptop",laptopController)
-app.use("/outdoor",outdoorController)
-app.use("/science",scienceController)
-app.use("/mobile",mobileController)
-app.use("/placeorder",placeorderController)
+app.use("/superDeal", superDealController)
+app.use("/addtocart", addtoCartController)
+app.use("/collection", userController)
+app.use("/all", allController)
+app.use("/kitchen", kitchenController)
+app.use("/laptop", laptopController)
+app.use("/outdoor", outdoorController)
+app.use("/science", scienceController)
+app.use("/mobile", mobileController)
+app.use("/placeorder", placeorderController)
 
-app.post("/register",register)
-app.post("/login",login)
+app.post("/register", register)
+app.post("/login", login)
 
 
 
-app.listen(port,async ()=>{
-    try{
+app.listen(port, async () => {
+    try {
         await connect();
         console.log("listing on port 7005")
-    }catch(err){
+    } catch (err) {
         console.log(err.message);
     }
-   
+
 })
